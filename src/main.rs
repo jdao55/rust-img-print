dd2407d164fbbf191ab1e45eb3f5051c5dd9b823
joe dao <joedao55@gmail.com>
2020-02-10 20:44:27 -0500

    added basic ascii conversion

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 extern crate image;
 
 use std::env;
-use image::ImageFormat;
 use image::{GenericImageView,imageops};
-use std::fs::File;
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let img = image::open(& args[1]).unwrap();
     let (x, y) = img.dimensions();
-    let img_scaled = img.resize(x/2, y/2, imageops::FilterType::Lanczos3);
-    let mut output= File::create("test2.png").unwrap();
-    img_scaled.write_to(&mut output, ImageFormat::Png).unwrap();
+    let dim_x = 80;
+    let dim_y =(y /(x/120))/2;
 
+    let img_scaled = img.resize(dim_x, dim_x, imageops::FilterType::Nearest);
+    let mut img_grey = imageops::colorops::grayscale(&img_scaled);
 
+    for (x, _, pixel) in img_grey.enumerate_pixels_mut() {
+        if pixel[0] > 127 {
+            print!("#");
+        }
+        else {
+            print!{" "};
+        }
+        if x == dim_x-1 {
+            print!("\n");
+        }
+    }
 }
