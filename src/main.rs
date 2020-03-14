f62d73baad806e104ea9e36fb961e827b7f52496
Joseph Dao <joedao55@gmail.com>
2020-03-14 19:23:46 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let img = image::open(& args[1]).unwrap();
     let (x, y) = img.dimensions();
+    //TODO have output dimensions as args
     let dim_x = 80;
     let dim_y =(y /(x/120))/2;
 
