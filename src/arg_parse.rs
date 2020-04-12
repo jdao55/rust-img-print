0f52802b4747f128d39652cd8cf5976b91c90fe3
joseph dao <joedao55@gmail.com>
2020-04-12 16:09:02 -0400

    added module for parsing cli args

---
+++ src/arg_parse.rs
@@ -0,0 +1,73 @@
+use clap::{App, Arg};
+pub struct ImageArgs {
+    pub filename: String,
+    pub width: u32,
+    pub height: Option<u32>,
+    pub greyscale: bool,
+    pub output_char: String,
+}
+impl ImageArgs {
+    pub fn new() -> ImageArgs {
+        let matches = App::new("img-print")
+            .version("1.0")
+            .author("Joseph Dao <joedao55@gmail.com>")
+            .about("")
+            .arg(
+                Arg::with_name("input")
+                    .help("input image file name")
+                    .required(true)
+                    .index(1),
+            )
+            .arg(
+                Arg::with_name("width")
+                    .help("output width(characters)")
+                    .required(false)
+                    .index(2),
+            )
+            .arg(
+                Arg::with_name("height")
+                    .help("output width(characters)")
+                    .required(false)
+                    .index(3),
+            )
+            .arg(
+                Arg::with_name("output_char")
+                    .help("output character")
+                    .required(false)
+                    .index(4),
+            )
+            .arg(
+                Arg::with_name("greyscale")
+                    .short("g")
+                    .long("greyscale")
+                    .help("turn on greyscale output"),
+            )
+            .get_matches();
+        let filename = matches.value_of("input").unwrap();
+
+        let width = match matches.value_of("width") {
+            Some(x) => x.parse::<u32>().unwrap_or(50),
+            None => 40,
+        };
+        let height: Option<u32> = match matches.value_of("height") {
+            Some(x) => Some(x.parse::<u32>().unwrap()),
+            None => None,
+        };
+        let greyscale = matches.is_present("greyscale");
+        let output_char = matches.value_of("output_char").unwrap_or("▇");
+        ImageArgs {
+            filename: String::from(filename),
+            width,
+            height,
+            greyscale,
+            output_char: String::from(output_char),
+        }
+    }
+
+    pub fn update_height(&mut self, x: u32, y: u32) {
+        self.height = match self.height {
+            Some(val) => Some(val),
+            None => Some(((f64::from(self.width) * f64::from(y) / f64::from(x)) / 2.15) as u32),
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,35 @@
-//extern crate image;
-//extern crate clap;
-
-use clap::{App, Arg};
-use image::{GenericImageView, imageops};
+use image::{imageops, GenericImageView};
+use img_print::arg_parse;
 use img_print::print_util;
 
 fn main() {
-    let matches = App::new("img-print")
-        .version("1.0")
-        .author("Joseph Dao <joedao55@gmail.com>")
-        .about("")
-        .arg(
-            Arg::with_name("input")
-                .help("input image file name")
-                .required(true)
-                .index(1))
-        .arg(
-            Arg::with_name("width")
-                .help("output width(characters)")
-                .required(false)
-                .index(2))
-        .arg(
-            Arg::with_name("height")
-                .help("output width(characters)")
-                .required(false)
-                .index(3))
-        .arg(
-            Arg::with_name("output_char")
-                .help("output character")
-                .required(false)
-                .index(4))
-        .arg(
-            Arg::with_name("greyscale")
-                .short("g")
-                .long("greyscale")
-                .help("turn on greyscale output")
-        )
-        .get_matches();
-    let name = matches.value_of("input").unwrap();
+    let mut args = arg_parse::ImageArgs::new();
 
-
-    let img = image::open(&name).unwrap();
+    let img = image::open(&args.filename).unwrap();
 
     let (x, y) = img.dimensions();
 
+    args.update_height(x, y);
 
-    let dim_x = match matches.value_of("width")
-    {
-        Some(x) => x.parse::<u32>().unwrap_or(50),
-        None => 40,
-    };
-    let dim_y = match matches.value_of("height")
-    {
-        Some(x) => x.parse::<u32>().unwrap(),
-        None => (( f64::from(dim_x)*f64::from(y)/ f64::from(x))/2.0) as u32
-    };
-
-    let output_char = matches.value_of("output_char").unwrap_or("▇");
-
-    let img_scaled = img.resize_exact(dim_x, dim_y, imageops::FilterType::CatmullRom);
+    let img_scaled = img.resize_exact(
+        args.width,
+        args.height.unwrap(),
+        imageops::FilterType::CatmullRom,
+    );
     let img_buffer = img_scaled.to_rgba();
-    if matches.is_present("greyscale")
-    {
-        img_buffer.enumerate_rows().for_each(
-            |(_, row)|{
-                row.for_each(|(_, _,pixel)| {print_util::print_char_gs(pixel, output_char);});
-                print!("\n");
+    if args.greyscale {
+        img_buffer.enumerate_rows().for_each(|(_, row)| {
+            row.for_each(|(_, _, pixel)| {
+                print_util::print_char_gs(pixel, &args.output_char);
             });
-    }
-    else
-    {
-        img_buffer.enumerate_rows().for_each(
-            |(_, row)|{
-                row.for_each(|(_, _,pixel)| {print_util::print_char_rgb(pixel, output_char);});
-                print!("\n");
+            print!("\n");
+        });
+    } else {
+        img_buffer.enumerate_rows().for_each(|(_, row)| {
+            row.for_each(|(_, _, pixel)| {
+                print_util::print_char_rgb(pixel, &args.output_char);
             });
+            print!("\n");
+        });
     }
 }
