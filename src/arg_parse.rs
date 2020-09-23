75e43d7905989c2c58c1b87e1dea2c7ea27b941b
joseph dao <joedao55@gmail.com>
2020-09-22 23:44:46 -0400

    added filtertype arguments

--- src/arg_parse.rs
+++ src/arg_parse.rs
@@ -1,10 +1,12 @@
 use clap::{App, Arg};
+use image::imageops::FilterType;
 pub struct ImageArgs {
     pub filename: String,
     pub width: u32,
     pub height: Option<u32>,
     pub greyscale: bool,
     pub output_char: String,
+    pub filtertype: FilterType,
 }
 impl ImageArgs {
     pub fn new() -> ImageArgs {
@@ -34,13 +36,21 @@ impl ImageArgs {
                 Arg::with_name("output_char")
                     .help("output character")
                     .required(false)
-                    .index(4),
+                    .takes_value(true),
             )
             .arg(
                 Arg::with_name("greyscale")
                     .short("g")
                     .long("greyscale")
                     .help("turn on greyscale output"),
+            )
+            .arg(
+                Arg::with_name("filtertype")
+                    .short("f")
+                    .required(false)
+                    .long("filtertype")
+                    .help("select scaling algorithm")
+                    .takes_value(true),
             )
             .get_matches();
         let filename = matches.value_of("input").unwrap();
@@ -55,12 +65,21 @@ impl ImageArgs {
         };
         let greyscale = matches.is_present("greyscale");
         let output_char = matches.value_of("output_char").unwrap_or("▇");
+        let filtertype = match matches.value_of("filtertype") {
+            Some("Nearest") => FilterType::Nearest,
+            Some("Triangle") => FilterType::Triangle,
+            Some("CatmullRom") => FilterType::CatmullRom,
+            Some("Gaussian") => FilterType::Gaussian,
+            Some("Lanczos3") => FilterType::Lanczos3,
+            _ => FilterType::Lanczos3,
+        };
         ImageArgs {
             filename: String::from(filename),
             width,
             height,
             greyscale,
             output_char: String::from(output_char),
+            filtertype,
         }
     }
 
